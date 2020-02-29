74f2f32498735de86a733c898a32b404c24361d7
lvyahui8 <lvyahui8@126.com>
2020-02-29 22:11:17 +0800

    optimize

--- feego-common-service-core/src/main/java/io/github/lvyahui8/core/lock/LockFactory.java
+++ feego-common-service-core/src/main/java/io/github/lvyahui8/core/lock/LockFactory.java
@@ -6,10 +6,14 @@ package io.github.lvyahui8.core.lock;
  */
 public interface LockFactory {
     /**
+     * 获取分布式锁实例
+     * - 此分布式锁并没有可重入特性。
+     * - 为保证进程crash后， 占用的分布式锁释放掉， 锁必须超时失效
+     * - 业务代码需要保证执行时间不超过锁的失效时间
      *
-     * @param lockKey
-     * @param ns
-     * @return
+     * @param lockKey 锁的唯一标识
+     * @param ns  每次为锁分配不同的值，以保证不会unlock掉别人的锁。如果没有很好的选择， 可以使用UUID
+     * @return 锁实例
      */
     DistributedLock newDistributeLock(String lockKey,Object ns) ;
 }
