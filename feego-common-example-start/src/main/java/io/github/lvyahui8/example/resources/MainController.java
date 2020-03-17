c9acc3af24e0b96d9752dee600a79eda336d3e00
lvyahui8 <lvyahui8@126.com>
2020-03-17 23:32:42 +0800

    锁示例

--- feego-common-example-start/src/main/java/io/github/lvyahui8/example/resources/MainController.java
+++ feego-common-example-start/src/main/java/io/github/lvyahui8/example/resources/MainController.java
@@ -1,9 +1,14 @@
 package io.github.lvyahui8.example.resources;
 
 import feego.common.io.github.lvyahui8.example.SystemLogger;
+import io.github.lvyahui8.core.lock.DistributedLock;
+import io.github.lvyahui8.core.lock.LockFactory;
 import io.github.lvyahui8.core.logging.LogSchema;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 /**
  * @author lvyahui (lvyahui8@gmail.com,lvyahui8@126.com)
@@ -12,6 +17,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 @RestController
 public class MainController {
+    private static int n = 0 ;
+
+    @Autowired
+    LockFactory lockFactory;
+
+    @RequestMapping("/update")
+    public Object update() {
+        DistributedLock lock = lockFactory.newDistributeLock("feego:common:update.key", UUID.randomUUID().toString());
+        boolean locked = false;
+        try {
+            if (locked = lock.tryLock()) {
+                SystemLogger.campaign.info("Got the lock");
+                return n++;
+            } else {
+                SystemLogger.campaign.warn("Didn't get the lock");
+                return 0;
+            }
+        } finally {
+            if (locked) {
+                lock.unlock();
+            }
+        }
+    }
+
     @RequestMapping("/status")
     public Object status() {
         SystemLogger.status.info(LogSchema.empty().of("status","ok"));
