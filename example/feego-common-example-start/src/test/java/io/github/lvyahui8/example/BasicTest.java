e1bc58b2cad2c8ab0e5d35a8daf2f0063f4c2094
lvyahui8 <lvyahui8@126.com>
2020-03-21 22:17:13 +0800

    update

--- example/feego-common-example-start/src/test/java/io/github/lvyahui8/example/BasicTest.java
+++ example/feego-common-example-start/src/test/java/io/github/lvyahui8/example/BasicTest.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.UUID;
+
 /**
  * @author lvyahui (lvyahui8@gmail.com,lvyahui8@126.com)
  * @since 2020/2/28 23:06
@@ -13,5 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @Slf4j
 public class BasicTest {
-
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID().toString());
+    }
 }
