7a284e2fe26f74faa23412f5fea472abe3f695a4
feego <lvyahui8@gmail.com>
2020-09-23 23:49:45 +0800

    测试模块重复日志能否正常工作

---
+++ feego-common-logging/feego-common-logging/src/test/java/io/github/lvyahui8/sdk/logging/RepeatModuleTestApplication.java
@@ -0,0 +1,19 @@
+package io.github.lvyahui8.sdk.logging;
+
+import io.github.lvyahui8.sdk.logging.configuration.DefaultLogConfiguration;
+import io.github.lvyahui8.sdk.logging.factory.LogbackModuleLoggerFactory;
+import io.github.lvyahui8.sdk.logging.schema.LogSchema;
+
+/**
+ * @author feego lvyahui8@gmail.com
+ * @date 2020/9/23
+ */
+public class RepeatModuleTestApplication {
+    public static void main(String[] args) {
+        LogbackModuleLoggerFactory factory = new LogbackModuleLoggerFactory(new DefaultLogConfiguration());
+        factory.initModuleLogger(TestLogger.class);
+        factory.initModuleLogger(TestLogger2.class);
+        TestLogger.db.info("from testLogger");
+        TestLogger2.db.info("from testLogger2");
+    }
+}
